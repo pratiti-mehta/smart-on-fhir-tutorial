f314f58b53f86568815c55035a6ebbc4a11bc768
pratiti-mehta <p.mehta2@nhs.net>
2026-08-19 13:47:49 +0100

    Add temperature data handling to example-smart-app

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -19,6 +19,7 @@
                               'http://loinc.org|2085-9', // HDL
                               'http://loinc.org|2089-1', //LDL
                               'http://loinc.org|85354-9' //Blood pressure
+                              'http://loinc.org|8310-5' // Temperature
                              ]
                       }
                     }
@@ -61,6 +62,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.tmp = getQuantityValueAndUnit(tmp[0]);
 
           ret.resolve(p);
         });
@@ -85,6 +87,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      tmp: {value: ''},
     };
   }
 
@@ -128,6 +131,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#tmp').html(p.tmp);
   };
 
 })(window);
